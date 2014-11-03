a7c495f0b6d324727432607193f3c9d6a91a2956
Daniel Oliveira <daniel@headdev.com.br>
2014-11-02 22:24:16 -0200

    first commit

---
+++ qtwebkitOLD/WebKitBuild/Debug/Source/JavaScriptCore/generated/DateConstructor.lut.h
@@ -0,0 +1,16 @@
+// Automatically generated from /home/daniel/developer/Headdev/qt/qtwebkit/Source/JavaScriptCore/runtime/DateConstructor.cpp using /home/daniel/developer/Headdev/qt/qtwebkit/Source/JavaScriptCore/create_hash_table. DO NOT EDIT!
+
+#include "Lookup.h"
+
+namespace JSC {
+
+static const struct HashTableValue dateConstructorTableValues[4] = {
+   { "parse", DontEnum|Function, (intptr_t)static_cast<NativeFunction>(dateParse), (intptr_t)1, NoIntrinsic },
+   { "UTC", DontEnum|Function, (intptr_t)static_cast<NativeFunction>(dateUTC), (intptr_t)7, NoIntrinsic },
+   { "now", DontEnum|Function, (intptr_t)static_cast<NativeFunction>(dateNow), (intptr_t)0, NoIntrinsic },
+   { 0, 0, 0, 0, NoIntrinsic }
+};
+
+extern const struct HashTable dateConstructorTable =
+    { 9, 7, dateConstructorTableValues, 0 };
+} // namespace
---
+++ qtwebkitOLD/WebKitBuild/Debug/Source/JavaScriptCore/generated/ErrorPrototype.lut.h
@@ -0,0 +1,14 @@
+// Automatically generated from /home/daniel/developer/Headdev/qt/qtwebkit/Source/JavaScriptCore/runtime/ErrorPrototype.cpp using /home/daniel/developer/Headdev/qt/qtwebkit/Source/JavaScriptCore/create_hash_table. DO NOT EDIT!
+
+#include "Lookup.h"
+
+namespace JSC {
+
+static const struct HashTableValue errorPrototypeTableValues[2] = {
+   { "toString", DontEnum|Function, (intptr_t)static_cast<NativeFunction>(errorProtoFuncToString), (intptr_t)0, NoIntrinsic },
+   { 0, 0, 0, 0, NoIntrinsic }
+};
+
+extern const struct HashTable errorPrototypeTable =
+    { 2, 1, errorPrototypeTableValues, 0 };
+} // namespace
